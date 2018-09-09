1185b16a4b84a951141e8ab71ec603de1ec830a5
dong <dongorigin@gmail.com>
2018-09-09 19:48:18 +0800

    new: longest common substring

---
+++ GrokkingAlgorithms/src/main/kotlin/LongestCommonSubstring.kt
@@ -0,0 +1,44 @@
+/**
+ * @author dong on 2018/09/09.
+ */
+
+/**
+ * a=hish b=fish
+ *
+ *   h i s h
+ * f 0 0 0 0
+ * i 0 0 0 0
+ * s 0 0 0 0
+ * h 0 0 0 0
+ *
+ * @return longest common substring length
+ */
+fun longestCommonSubstring(wordA: String, wordB: String): Int {
+    val grid: Array<Array<Int>> = Array(wordA.length) { Array(wordB.length) { 0 } }
+
+    var longestLength = 0
+    for (i in 0 until wordA.length) {
+        for (j in 0 until wordB.length) {
+            if (wordA[i] == wordB[j]) {
+                val previousLength = if (i > 0 && j > 0) {
+                    grid[i - 1][j - 1]
+                } else {
+                    0
+                }
+                val newLength = previousLength + 1
+                grid[i][j] = newLength
+                if (longestLength < newLength) {
+                    longestLength = newLength
+                }
+            } else {
+                grid[i][j] = 0
+            }
+        }
+    }
+    return longestLength
+}
+
+fun main(args: Array<String>) {
+    println(longestCommonSubstring("hish", "fish"))
+    println(longestCommonSubstring("hish", "vista"))
+}
