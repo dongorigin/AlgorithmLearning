e36bf97d7b35c7a610b90636baefac77eb2f75dc
dong <dongorigin@gmail.com>
2018-10-27 21:02:47 +0800

    new: top k

---
+++ CodingInterviews/src/main/kotlin/cn/dong/interviews/TopK.java
@@ -0,0 +1,48 @@
+package cn.dong.interviews;
+
+/**
+ * @author dong on 2018/10/27.
+ */
+public class TopK {
+    public static int topK(int[] numbers, int k) {
+        if (k < 0 || k >= numbers.length) {
+            return -1;
+        }
+        int start = 0;
+        int end = numbers.length - 1;
+        int pivot = partition(numbers, start, end);
+        while (pivot + 1 != k) {
+            if (pivot + 1 > k) {
+                pivot = partition(numbers, start, pivot - 1);
+            } else {
+                // pivot + 1 < k
+                pivot = partition(numbers, pivot + 1, end);
+            }
+        }
+        return numbers[pivot];
+    }
+
+    private static int partition(int[] numbers, int start, int end) {
+        int pivot = numbers[end];
+        int i = start; // [start, i-1] 的区间是大于 pivot 的元素
+        for (int j = start; j < end; j++) {
+            if (numbers[j] > pivot) {
+                swap(numbers, i, j);
+                i++;
+            }
+        }
+        swap(numbers, i, end);
+        return i;
+    }
+
+    private static void swap(int[] numbers, int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {5, 4, 9, 11, 6, 3, 8};
+        System.out.println(topK(input, 3));
+    }
+}
