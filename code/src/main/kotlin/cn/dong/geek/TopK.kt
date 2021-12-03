027250376d3171892c6d4bb788191823bd519700
dong <dongorigin@gmail.com>
2021-12-03 17:02:37 +0800

    top k by prority queue

--- code/src/main/kotlin/cn/dong/geek/TopK.kt
+++ code/src/main/kotlin/cn/dong/geek/TopK.kt
@@ -1,5 +1,7 @@
 package cn.dong.geek
 
+import java.util.*
+
 /**
  * @author dong on 2021/12/03.
  */
@@ -25,11 +27,26 @@ fun IntArray.topK(k: Int): IntArray {
     return result
 }
 
+fun IntArray.topK2(k: Int): IntArray {
+    val priorityQueue = PriorityQueue<Int>(k)
+    for (i in this) {
+        if (priorityQueue.size < k) {
+            priorityQueue.offer(i)
+        } else {
+            if (i > priorityQueue.peek()) {
+                priorityQueue.poll()
+                priorityQueue.offer(i)
+            }
+        }
+    }
+    return priorityQueue.toIntArray()
+}
+
 fun main() {
-    intArrayOf(1, 2, 3, 4, 5).topK(3).print()
-    intArrayOf(1, 1, 1, 1, 1).topK(3).print()
-    intArrayOf(5, 4, 3, 2, 1).topK(3).print()
-    intArrayOf(4, 3, 5, 1, 2).topK(3).print()
+    intArrayOf(1, 2, 3, 4, 5).topK2(3).print()
+    intArrayOf(1, 1, 1, 1, 1).topK2(3).print()
+    intArrayOf(5, 4, 3, 2, 1).topK2(3).print()
+    intArrayOf(4, 3, 5, 1, 2).topK2(3).print()
 }
 
 private fun IntArray.print() {
