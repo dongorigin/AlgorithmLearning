861e224895ac32b65f1cb81eec801e346acc833b
dong <dongorigin@gmail.com>
2021-12-11 12:40:55 +0800

    01package

---
+++ code/src/main/kotlin/cn/dong/geek/01Package.kt
@@ -0,0 +1,59 @@
+package cn.dong.geek
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.IsStableType
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+import java.lang.Integer.max
+
+/**
+ * 我们有一个背包，背包总的承载重量是 [maxWeight] kg。现在我们有 n 个物品，每个物品的重量不等，并且不可分割。[itemWeights] 为 n 个物品的重量
+ * 我们现在期望选择几件物品，装载到背包中。在不超过背包所能装载重量的前提下，如何让背包中物品的总重量最大？
+ *
+ * @author dong on 2021/12/11.
+ */
+class Package(
+    private val itemWeights: IntArray,
+    private val maxWeight: Int,
+) {
+    var currentMax = 0
+
+    fun findMax(): Int {
+        innerFind(maxWeight, 0)
+        return currentMax
+    }
+
+    /** [index] 待定物品下标 */
+    private fun innerFind(remainingWeight: Int, index: Int) {
+        currentMax = max(currentMax, maxWeight - remainingWeight)
+        if (index >= itemWeights.size) return
+
+        // 装
+        if (itemWeights[index] <= remainingWeight) { // 剪枝
+            innerFind(remainingWeight - itemWeights[index], index + 1)
+        }
+        // 不装
+        innerFind(remainingWeight, index + 1)
+    }
+
+}
+
+private class PackageTests : FunSpec({
+    @IsStableType
+    data class Case(
+        val itemWeights: IntArray,
+        val maxWeight: Int,
+        val expectedMax: Int,
+    )
+
+    context("test find max") {
+        withData(
+            Case(intArrayOf(8, 4, 2), 1, 0),
+            Case(intArrayOf(8, 4, 2), 3, 2),
+            Case(intArrayOf(8, 4, 2), 10, 10),
+            Case(intArrayOf(2, 4, 8), 10, 10)
+        ) { (itemWeights, maxWeight, expectedMax) ->
+            Package(itemWeights, maxWeight).findMax() shouldBe expectedMax
+        }
+    }
+})
