c8aeae9ce99f2b4237021cf57fe25d83a4c58427
dong <dongorigin@gmail.com>
2021-12-14 19:52:55 +0800

    reverse between

--- code/src/main/kotlin/cn/dong/leetcode/209.reverse-linked-list.kt
+++ code/src/main/kotlin/cn/dong/leetcode/209.reverse-linked-list.kt
@@ -7,6 +7,8 @@ package cn.dong.leetcode
  */
 private class ListNode(val value: Int) {
     var next: ListNode? = null
+
+    override fun toString(): String = value.toString()
 }
 
 private fun reverseList(head: ListNode?): ListNode? {
@@ -32,4 +34,52 @@ private fun reverseListByRecursion(head: ListNode?): ListNode? {
     } else {
         head
     }
+}
+
+/** https://leetcode-cn.com/problems/reverse-linked-list-ii/ */
+private fun reverseBetween(head: ListNode?, left: Int, right: Int): ListNode? {
+    val dummy = ListNode(-1)
+    dummy.next = head
+
+    var leftPrev = dummy
+    var current = dummy.next
+    var i = 0
+    while (current != null) {
+        i++
+        if (i < left) {
+            leftPrev = current
+            current = current.next
+        } else if (i < right) {
+            val removed = current.next
+            current.next = removed?.next
+
+            removed?.next = leftPrev.next
+            leftPrev.next = removed
+        } else {
+            break
+        }
+    }
+    return dummy.next
+}
+
+private fun reverseBetween2(head: ListNode?, left: Int, right: Int): ListNode? {
+    val dummy = ListNode(-1)
+    dummy.next = head
+
+    var leftPrev: ListNode? = dummy
+    var current: ListNode? = dummy.next
+
+    for (i in 1 until left) {
+        leftPrev = current
+        current = current?.next
+    }
+
+    for (i in 1..right - left) {
+        val removed = current?.next
+        current?.next = current?.next?.next
+
+        removed?.next = leftPrev?.next
+        leftPrev?.next = removed
+    }
+    return dummy.next
 }
