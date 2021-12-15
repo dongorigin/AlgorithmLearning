bf2ad93795f5685ebd8b753c1ef50d289a814def
dong <dongorigin@gmail.com>
2021-12-16 00:10:22 +0800

    fix import

--- code/src/main/kotlin/cn/dong/leetcode/SearchInRotatedSortedArray.java
+++ code/src/main/kotlin/cn/dong/leetcode/SearchInRotatedSortedArray.java
@@ -1,6 +1,6 @@
 package cn.dong.leetcode;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * https://leetcode.com/problems/search-in-rotated-sorted-array
