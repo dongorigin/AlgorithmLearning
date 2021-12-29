8881407c18ea44c7624de8f78839835cbe51ea7d
dong <dongorigin@gmail.com>
2021-12-29 15:27:02 +0800

    remove-all-adjacent-duplicates-in-string

---
+++ code/src/main/kotlin/cn/dong/leetcode/1047.remove-all-adjacent-duplicates-in-string.kt
@@ -0,0 +1,20 @@
+package cn.dong.leetcode
+
+/**
+ * [1047. 删除字符串中的所有相邻重复项 - 力扣（LeetCode）](https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/)
+ *
+ * @author dong on 2021/12/29.
+ */
+class Solution {
+    fun removeDuplicates(s: String): String {
+        val sb = StringBuilder()
+        for (char in s) {
+            if (sb.lastOrNull() == char) {
+                sb.deleteCharAt(sb.length - 1)
+            } else {
+                sb.append(char)
+            }
+        }
+        return sb.toString()
+    }
+}
