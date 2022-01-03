74e8adbbdd3bf6464c992c4711ab0c61c61098ad
dong <dongorigin@gmail.com>
2022-01-03 11:44:22 +0800

    reverse-linked-list-ii

--- code/src/main/kotlin/cn/dong/leetcode/92.reverse-linked-list-ii.kt
+++ code/src/main/kotlin/cn/dong/leetcode/92.reverse-linked-list-ii.kt
@@ -1,52 +1,29 @@
 package cn.dong.leetcode
+
 /**
  * [92. 反转链表 II - 力扣（LeetCode）](https://leetcode-cn.com/problems/reverse-linked-list-ii/)
  *
  * @author dong on 2021/12/13.
  */
-private fun reverseBetween(head: ListNode?, left: Int, right: Int): ListNode? {
-    val dummy = ListNode(-1)
-    dummy.next = head
+class Solution92 {
+    fun reverseBetween(head: ListNode?, left: Int, right: Int): ListNode? {
+        val dummy = ListNode(-1)
+        dummy.next = head
 
-    var leftPrev = dummy
-    var current = dummy.next
-    var i = 0
-    while (current != null) {
-        i++
-        if (i < left) {
-            leftPrev = current
-            current = current.next
-        } else if (i < right) {
-            val removed = current.next
-            current.next = removed?.next
+        var leftPrev = dummy
+        var cur = dummy.next
 
-            removed?.next = leftPrev.next
-            leftPrev.next = removed
-        } else {
-            break
+        for (i in 1..left - 1) {
+            leftPrev = leftPrev.next!!
+            cur = cur?.next
         }
+
+        for (i in 1..right - left) {
+            val removed = cur!!.next!!
+            cur.next = removed.next
+            removed.next = leftPrev.next
+            leftPrev.next = removed
+        }
+        return dummy.next
     }
-    return dummy.next
-}
-
-private fun reverseBetween2(head: ListNode?, left: Int, right: Int): ListNode? {
-    val dummy = ListNode(-1)
-    dummy.next = head
-
-    var leftPrev: ListNode? = dummy
-    var current: ListNode? = dummy.next
-
-    for (i in 1 until left) {
-        leftPrev = current
-        current = current?.next
-    }
-
-    for (i in 1..right - left) {
-        val removed = current?.next
-        current?.next = current?.next?.next
-
-        removed?.next = leftPrev?.next
-        leftPrev?.next = removed
-    }
-    return dummy.next
 }
