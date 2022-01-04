bb299ed3fa47b9e5a2b3da4b968c234529aff94a
dong <dongorigin@gmail.com>
2022-01-04 22:26:06 +0800

    balanced-binary-tree

--- code/src/main/kotlin/cn/dong/leetcode/110.balanced-binary-tree.kt
+++ code/src/main/kotlin/cn/dong/leetcode/110.balanced-binary-tree.kt
@@ -1,24 +1,77 @@
 package cn.dong.leetcode
 
+import io.kotest.matchers.shouldBe
+
 /**
  * [110. 平衡二叉树 - 力扣（LeetCode）](https://leetcode-cn.com/problems/balanced-binary-tree/)
  *
  * @author dong on 2021/12/22.
  */
 class Solution110 {
+    var count = 0
+
+    /** 后序遍历，从底至顶处理子树，同时算子树的高度和判断平衡，避免了重复遍历，时间复杂度 O(n) */
+    fun isBalanced(root: TreeNode?): Boolean {
+        return height(root) >= 0
+    }
+
+    /** 返回树的高度，同时判断树是否平衡，不平衡返回 -1。因为 */
+    fun height(root: TreeNode?): Int {
+        if (root == null) return 0
+        count++
+        val leftH = height(root.left)
+        if (leftH < 0) return -1
+
+        val rightH = height(root.right)
+        if (rightH < 0) return -1
+
+        if (Math.abs(leftH - rightH) > 1) return -1
+        return 1 + Math.max(leftH, rightH)
+    }
+}
+
+class Solution110_2 {
+    var count = 0
+
+    /** 后序遍历，但是对每个子树会重复调用多次 height，时间复杂度 O(n log n) */
     fun isBalanced(root: TreeNode?): Boolean {
         if (root == null) return true
         return isBalanced(root.left)
                 && isBalanced(root.right)
-                && abs((height(root.left) - height(root.right))) <= 1
+                && Math.abs((height(root.left) - height(root.right))) <= 1
     }
-
-    private fun abs(i: Int): Int = if (i >= 0) i else -i
-
-    private fun max(i: Int, j: Int): Int = if (i > j) i else j
 
     private fun height(root: TreeNode?): Int {
         if (root == null) return 0
-        return 1 + max(height(root.left), height(root.right))
+        count++
+        return 1 + Math.max(height(root.left), height(root.right))
     }
+}
+
+fun main() {
+    val root = TreeNode(1)
+    root.left = TreeNode(2).apply {
+        left = TreeNode(4).apply {
+            left = TreeNode(8)
+        }
+        right = TreeNode(5)
+    }
+    root.right = TreeNode(3).apply {
+        left = TreeNode(6).apply {
+            right = TreeNode(13)
+        }
+        right = TreeNode(7).apply {
+            left = TreeNode(14).apply {
+                left = TreeNode(28)
+            }
+            right = TreeNode(15)
+        }
+    }
+
+    val solution110 = Solution110()
+    solution110.isBalanced(root) shouldBe true
+    println("first ${solution110.count}")
+    val solution1102 = Solution110_2()
+    solution1102.isBalanced(root) shouldBe true
+    println("second ${solution1102.count}")
 }
