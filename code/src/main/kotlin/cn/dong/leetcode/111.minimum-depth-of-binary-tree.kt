afb0279ec440c3c83544748ed40681477c0315f6
dong <dongorigin@gmail.com>
2022-01-04 22:25:32 +0800

    minimum-depth-of-binary-tree

---
+++ code/src/main/kotlin/cn/dong/leetcode/111.minimum-depth-of-binary-tree.kt
@@ -0,0 +1,20 @@
+package cn.dong.leetcode
+
+/**
+ * [111. 二叉树的最小深度 - 力扣（LeetCode）](https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/)
+ *
+ * @author dong on 2022/01/04.
+ */
+class Solution111 {
+    fun minDepth(root: TreeNode?): Int {
+        return when {
+            root == null -> 0
+            root.left == null && root.right == null -> 1
+            root.left == null && root.right != null -> 1 + minDepth(root.right)
+            root.left != null && root.right == null -> 1 + minDepth(root.left)
+            else -> 1 + min(minDepth(root.left), minDepth(root.right))
+        }
+    }
+
+    fun min(i: Int, j: Int) = if (i < j) i else j
+}
