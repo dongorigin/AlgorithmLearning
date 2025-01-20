e50940e925528e78009f4d2b163e20d56d5265f0
dong <dongorigin@gmail.com>
2025-01-20 13:22:13 +0800

    21.merge-two-sorted-lists

--- code/src/main/kotlin/cn/dong/leetcode/21.merge-two-sorted-lists.kt
+++ code/src/main/kotlin/cn/dong/leetcode/21.merge-two-sorted-lists.kt
@@ -8,25 +8,25 @@ package cn.dong.leetcode
 class Solution21 {
 
     fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
-        var l1 = list1
-        var l2 = list2
+        var p1 = list1
+        var p2 = list2
         val preHead = ListNode(-1)
-        var prev = preHead
-        while (l1 != null && l2 != null) {
-            if (l1.`val` < l2.`val`) {
-                prev.next = l1
-                prev = l1
-                l1 = l1.next
+        var tail = preHead
+        while (p1 != null && p2 != null) {
+            if (p1.`val` < p2.`val`) {
+                tail.next = p1
+                tail = p1
+                p1 = p1.next
             } else {
-                prev.next = l2
-                prev = l2
-                l2 = l2.next
+                tail.next = p2
+                tail = p2
+                p2 = p2.next
             }
         }
-        if (l1 == null) {
-            prev.next = l2
-        } else {
-            prev.next = l1
+        if (p1 == null) { // list1 is end
+            tail.next = p2
+        } else if (p2 == null) { // list2 is end
+            tail.next = p1
         }
         return preHead.next
     }
@@ -45,5 +45,52 @@ class Solution21 {
                 list2
             }
         }
+    }
+
+    /**
+     * 不使用 dummy/哨兵 头节点，手动处理 head 为空的情况
+     */
+    fun mergeTwoLists_noDummy(list1: ListNode?, list2: ListNode?): ListNode? {
+        var head: ListNode? = null
+        var tail: ListNode? = null
+        var p1 = list1
+        var p2 = list2
+        while (p1 != null && p2 != null) {
+            if (p1.`val` <= p2.`val`) {
+                // move p1 to new list
+                if (tail == null) {
+                    head = p1
+                } else {
+                    tail.next = p1
+                }
+                tail = p1
+                p1 = p1.next
+                tail.next = null
+            } else {
+                // move p2 to new list
+                if (tail == null) {
+                    head = p2
+                } else {
+                    tail.next = p2
+                }
+                tail = p2
+                p2 = p2.next
+                tail.next = null
+            }
+        }
+        if (p1 == null) { // list1 is end
+            if (tail == null) {
+                head = p2
+            } else {
+                tail.next = p2
+            }
+        } else if (p2 == null) { // list2 is end
+            if (tail == null) {
+                head = p1
+            } else {
+                tail.next = p1
+            }
+        }
+        return head
     }
 }
