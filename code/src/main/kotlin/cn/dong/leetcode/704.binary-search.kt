06438d03514a8fdbb428757397364f65dd7f0390
dong <dongorigin@gmail.com>
2025-01-23 19:30:11 +0800

    704 binary search

--- code/src/main/kotlin/cn/dong/leetcode/704.binary-search.kt
+++ code/src/main/kotlin/cn/dong/leetcode/704.binary-search.kt
@@ -1,5 +1,7 @@
 package cn.dong.leetcode
 
+import kotlin.test.assertEquals
+
 /**
  * [704. 二分查找 - 力扣（LeetCode）](https://leetcode-cn.com/problems/binary-search/)
  *
@@ -23,4 +25,32 @@ class Solution704 {
         }
         return -1
     }
+}
+
+class Solution704_Recursion {
+
+    fun search(nums: IntArray, target: Int): Int {
+        return binarySearch(nums, target, 0, nums.size - 1)
+    }
+
+    private fun binarySearch(nums: IntArray, target: Int, start: Int, end: Int): Int {
+        if (start > end) return -1
+
+        val midIndex = start + (end - start) / 2
+        val mid = nums[midIndex]
+        if (target == mid) {
+            return midIndex
+        } else if (target < mid) {
+            return binarySearch(nums, target, start, midIndex - 1)
+        } else {
+            return binarySearch(nums, target, midIndex + 1, end)
+        }
+    }
+}
+
+fun main() {
+    val array = intArrayOf(-1,0,3,5,9,12)
+
+    assertEquals(4, Solution704_Recursion().search(array, 9))
+    assertEquals(-1, Solution704_Recursion().search(array, 2))
 }
