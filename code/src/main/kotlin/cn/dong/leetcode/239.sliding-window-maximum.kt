aabea7b3d8796d79ae22760f9903c8d961828161
dong <dongorigin@gmail.com>
2025-03-06 17:47:21 +0800

    239.sliding-window-maximum

--- code/src/main/kotlin/cn/dong/leetcode/239.sliding-window-maximum.kt
+++ code/src/main/kotlin/cn/dong/leetcode/239.sliding-window-maximum.kt
@@ -1,7 +1,6 @@
 package cn.dong.leetcode
 
 import io.kotest.matchers.shouldBe
-import java.util.*
 
 /**
  * [239. 滑动窗口最大值 - 力扣（LeetCode）](https://leetcode-cn.com/problems/sliding-window-maximum/)
@@ -14,39 +13,33 @@ import java.util.*
  * 单调队列的性质
  * - 单调递减，队首是最大的。
  * - 添加元素，保持单调递减，添加前移除比当前小的元素。
- * - 删掉队首，队首还是最大的。因为可能已经在添加新元素时被移除了，所以要确认队首是待移除的。所以存下标更直观。但存下标作为独立数据结构很奇怪
- *
- * 使用 [ArrayDeque] 时速度慢，无法通过用例，很奇怪，ArrayDeque 是用循环队列实现的，应该也是 O(1)，和 [LinkedList] 无区别才对
- *
- * @author dong on 2022/01/01.
+ * - 删掉队首，队首还是最大的。因为可能已经在添加新元素时被移除了，所以要确认队首是待移除的。
  */
 class Solution239 {
     fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
+        val deque = ArrayDeque<Int>() // 队列保持递减，队首是最大的
         val result = IntArray(nums.size - k + 1)
-        val indexDeque: Deque<Int> = LinkedList() // index 递增，nums[index] 递减
+        for (end in nums.indices) {
+            // i=end=start+k-1, start=i-k+1
+            val start = end - k + 1
 
-        val push: (index: Int) -> Unit = {
-            while (indexDeque.isNotEmpty() && nums[indexDeque.last()] < nums[it]) {
-                indexDeque.removeLast()
+            // in
+            while (deque.isNotEmpty() && deque.last() < nums[end]) {
+                deque.removeLast()
             }
-            indexDeque.addLast(it)
-        }
+            deque.addLast(nums[end])
 
-        for (i in 0..k - 2) {
-            push(i)
-        }
-
-        for (start in 0..nums.size - k) {
-            // window start
-            while (indexDeque.isNotEmpty() && indexDeque.first() < start) {
-                indexDeque.removeFirst()
+            // out
+            if (start > 0) {
+                if (deque.first() == nums[start - 1]) {
+                    deque.removeFirst()
+                }
             }
 
-            // window end
-            val end = start + k - 1
-            push(end)
-
-            result[start] = nums[indexDeque.first()]
+            // update
+            if (start >= 0) {
+                result[start] = deque.first()
+            }
         }
         return result
     }
